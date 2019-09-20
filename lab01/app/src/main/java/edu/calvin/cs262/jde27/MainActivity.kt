5b560fd678d97e1c4fa98a6b4e61030b29139dd2
Jonathan Ellis <jde27@students.calvin.edu>
2019-09-20 14:51:54 -0400

    Add lab01

---
+++ lab01/app/src/main/java/edu/calvin/cs262/jde27/MainActivity.kt
@@ -0,0 +1,14 @@
+package edu.calvin.cs262.jde27
+
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Log.d("MainActivity", "Hello jde27")
+    }
+}
