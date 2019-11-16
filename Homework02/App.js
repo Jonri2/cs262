564f00fb18d9cf5c1999a0ffa7d115947547560b
Jonathan Ellis <jde27@students.calvin.edu>
2019-11-15 20:00:52 -0500

    Format Homework02

--- Homework02/App.js
+++ Homework02/App.js
@@ -1,10 +1,15 @@
-import React, {Component} from 'react';
-import { View, Text, Button } from 'react-native';
-import { createAppContainer } from 'react-navigation';
-import { createStackNavigator } from 'react-navigation-stack';
+import React, { Component } from 'react'
+import { View, Text, Button } from 'react-native'
+import { createAppContainer } from 'react-navigation'
+import { createStackNavigator } from 'react-navigation-stack'
 
+/*
+  HomeScreen is a basic default screen for an app
+  Returns: a View component with text displaying "Home Screen"
+            and a button that sends the user to a secondary page
+*/
 class HomeScreen extends Component {
-  render() {
+  render () {
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
         <Text>Home Screen</Text>
@@ -13,13 +18,18 @@ class HomeScreen extends Component {
           onPress={() => this.props.navigation.navigate('Details')}
         />
       </View>
-    );
+    )
   }
 }
 
-
+/*
+  DetailsScreen is a basic secondary screen for an app.
+  Returns: a View component with text displaying "Details Screen"
+            and three buttons that send the user to the home screen, 
+            back to the previous page, or to the home screen
+*/
 class DetailsScreen extends Component {
-  render() {
+  render () {
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
         <Text>Details Screen</Text>
@@ -36,24 +46,30 @@ class DetailsScreen extends Component {
           onPress={() => this.props.navigation.goBack()}
         />
       </View>
-    );
+    )
   }
 }
 
+// Initialize the app navigator
 const AppNavigator = createStackNavigator(
   {
     Home: HomeScreen,
-    Details: DetailsScreen,
+    Details: DetailsScreen
   },
   {
-    initialRouteName: 'Home',
+    initialRouteName: 'Home'
   }
-);
+)
 
-const AppContainer = createAppContainer(AppNavigator);
+const AppContainer = createAppContainer(AppNavigator)
 
+/*
+  The App component puts bot the Home and Detail Screens in
+  a container with the AppNavigator
+  Returns: The App Container
+*/
 export default class App extends React.Component {
-  render() {
-    return <AppContainer />;
+  render () {
+    return <AppContainer />
   }
 }
